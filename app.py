751e3e2b7587cf093810a69ff6e4d5776d0bbd3d
Ricardo Rocha <ricardo8610@gmail.com>
2025-11-13 10:28:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title('Meu primeiro App ❤')
+
+st.header('Vamos fazer algo com interatividade!')
+
+n = st.number_input('Entre com um numero')
+
+st.write(f'O número que você escolheu ao quadrado é {n**2}')
